0427ee0a009a64128051e95099b41930c02e6b52
Manav Gupta <37526340+manav98@users.noreply.github.com>
2024-09-08 16:32:06 +0530

    Added welcome method

--- src/main/java/com/telusko/SpringSecEx/HelloController.java
+++ src/main/java/com/telusko/SpringSecEx/HelloController.java
@@ -1,5 +1,6 @@
 package com.telusko.SpringSecEx;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("")
-    public String greet() {
-        return "Welcome to Telusko";
+    public String greet(HttpServletRequest request) {
+        String str = String.valueOf(request);
+        return "Welcome to Telusko " + request.getSession().getId();
     }
 }
